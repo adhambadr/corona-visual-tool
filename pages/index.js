8713cd1a428d3b0531a96359544e20f103e5ecea
adhambadr <adhambadr2@gmail.com>
2020-03-16 22:27:43 +0100

    Colors etc

--- pages/index.js
+++ pages/index.js
@@ -164,13 +164,13 @@ export default class Home extends Component {
 const backgrounds = [
   "rgba(255, 99, 132, 0.2)",
   "rgba(54, 162, 235, 0.2)",
-  "rgba(255, 206, 86, 0.2)",
+  "rgba(0, 0, 0, 0.2)",
   "rgba(75, 192, 192, 0.2)"
 ];
 const borders = [
   "rgba(255, 99, 132, 1)",
   "rgba(54, 162, 235, 1)",
-  "rgba(255, 206, 86, 1)",
+  "rgba(0, 0, 0, 1)",
   "rgba(75, 192, 192, 1)",
   "rgba(153, 102, 255, 1)",
   "rgba(255, 159, 64, 1)"
