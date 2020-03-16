41c490a5785a8d2fc51f8486d6f58d984bea01a4
adhambadr <adhambadr2@gmail.com>
2020-03-16 21:09:10 +0100

    Data Labels

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,22 @@ import axios from "axios";
 import _ from "lodash";
 import Chart from "chart.js";
 import moment from "moment";
+import numeral from "numeral";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+// numeral.register("locale", "de", {
+//   delimiters: {
+//     thousands: ".",
+//     decimal: ","
+//   }
+// });
+// numeral.locale("de");
 export default class Home extends Component {
   static async getInitialProps({ query }) {
     const country = query.country || "Germany";
-    const url = "http://localhost:2019/historic?country=" + country;
+    const url =
+      (process.env.NODE_ENV === "production"
+        ? "https://corona.blloc.com/history?country="
+        : "http://localhost:2019/historic?country=") + country;
     const { data } = await axios.get(url);
     return {
       data,
@@ -14,8 +26,20 @@ export default class Home extends Component {
     };
   }
   state = {
-    selectedState: "federal"
+    selectedState: "federal",
+    options: ["confirmed", "recovered", "deaths"]
   };
+  getDataSets = () =>
+    _.map(this.state.options, (option, i) => ({
+      label: "# " + option + " cases",
+      data: _.map(
+        _.get(this.props, "data." + this.state.selectedState, []),
+        option
+      ),
+      backgroundColor: backgrounds[i],
+      borderColor: borders[i],
+      borderWidth: 1
+    }));
   chartParams = {
     type: "line",
     data: {
@@ -23,22 +47,21 @@ export default class Home extends Component {
         _.get(this.props, "data." + this.state.selectedState, []),
         ({ date }) => new moment(date).format("DD.MM.YY")
       ),
-      datasets: [
-        {
-          label: "# of Confirmed cases",
-          data: _.map(
-            _.get(this.props, "data." + this.state.selectedState, []),
-            "confirmed"
-          ),
-          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)"],
-          borderWidth: 1
-        }
-      ]
+      datasets: this.getDataSets()
     },
     options: {
-      gridLines: {
-        display: false
+      title: {
+        display: true,
+        text: "Covid-19 Historic data for " + this.props.country
+      },
+      tooltips: {
+        enabled: true
+      },
+      plugins: {
+        datalabels: {
+          align: "top",
+          formatter: (val, context) => numeral(val).format("0,0")
+        }
       },
       scales: {
         xAxes: [
@@ -51,7 +74,8 @@ export default class Home extends Component {
         yAxes: [
           {
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
+              callback: (label, index, labels) => numeral(label).format("0,0")
             },
             gridLines: {
               display: false
@@ -67,8 +91,35 @@ export default class Home extends Component {
   };
   render = () => (
     <div>
-      <h4>{this.props.country}</h4>
       <canvas i ref={r => (this.ctx = r)} width="400" height="400" />
     </div>
   );
+
+  select = () => (
+    <select
+      onChange={({ target }) => this.setState({ selectedState: target.value })}
+    >
+      <option value="federal">all</option>
+      {_.map(_.sortBy(_.keys(_.get(this.props, "data"))), (state, i) => (
+        <option key={i} value={state}>
+          {state}
+        </option>
+      ))}
+    </select>
+  );
 }
+
+const backgrounds = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)"
+];
+const borders = [
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)"
+];
